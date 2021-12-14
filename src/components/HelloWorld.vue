9ad53e4e56914dcd55b6401a4f9041bb5dff76be
余乾 <yuqian@ergengtv.com>
2021-12-14 11:08:06 +0800

    chore: 增加配置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import {ref} from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{msg: string}>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -19,9 +19,7 @@ const count = ref(0)
   <p>See <code>README.md</code> for more information.</p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
